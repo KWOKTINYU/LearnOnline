0fab07dc772ad5b238e16227a6e914dfb16fc69f
KWOKTINYU <tykwok665@hotmail.com>
2017-05-04 23:00:09 +0800

    5/4

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -11,7 +11,7 @@ class Course(models.Model):
     name = models.CharField(max_length=50, verbose_name=u"课程名称")
     desc = models.CharField(max_length=300, verbose_name=u"课程描述")
     detail = models.TextField(verbose_name=u"课程详情")
-    degree = models.CharField(choices=(("primary", u"初级"), ("middle", u"中级"), ("senior", u"高级")), max_length=2)
+    degree = models.CharField(verbose_name=u"难度", choices=(("primary", u"初级"), ("middle", u"中级"), ("senior", u"高级")), max_length=7)
     learn_times = models.IntegerField(default=0, verbose_name=u"学习时长(分钟)")
     learn_students = models.IntegerField(default=0, verbose_name=u"学习人数")
     fav_nums = models.IntegerField(default=0, verbose_name=u"收藏人数")
@@ -22,6 +22,9 @@ class Course(models.Model):
     class Meta:
         verbose_name = u"课程"
         verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.name
 
 
 class Lesson(models.Model):
